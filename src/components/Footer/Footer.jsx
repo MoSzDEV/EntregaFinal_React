e4f3c8cc568f9e15e7d144b759beb2810fd649d1
Mariano E. Sanchez <115333392+MoSzDEV@users.noreply.github.com>
2023-05-11 01:19:35 -0300

    Update Footer.jsx
    Correction class for className to "template Footer"

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,11 +9,11 @@ const Footer = () => {
             <span className="copyright">
                 Powered by MoSz
             </span>
-            <section class="buttons">
-                <a target="_blank" href="https://translate.google.com.ar/?hl=es&sl=en&tl=es&text=test%20link&op=translate" class="fa fa-facebook"></a>
-                <a target="_blank" href="https://translate.google.com.ar/?hl=es&sl=en&tl=es&text=test%20link&op=translate" class="fa fa-twitter"></a>
-                <a target="_blank" href="https://www.youtube.com/watch?v=T_j60n1zgu0&list=PLV8x_i1fqBw0B008sQn79YxCjkHJU84pC" class="fa fa-youtube"></a>
-                <a target="_blank" href="https://wa.me/message/SSVLSRGQXZSLN1" class="fa fa-whatsapp"></a>
+            <section className="buttons">
+                <a target="_blank" href="https://translate.google.com.ar/?hl=es&sl=en&tl=es&text=test%20link&op=translate" className="fa fa-facebook"></a>
+                <a target="_blank" href="https://translate.google.com.ar/?hl=es&sl=en&tl=es&text=test%20link&op=translate" className="fa fa-twitter"></a>
+                <a target="_blank" href="https://www.youtube.com/watch?v=T_j60n1zgu0&list=PLV8x_i1fqBw0B008sQn79YxCjkHJU84pC" className="fa fa-youtube"></a>
+                <a target="_blank" href="https://wa.me/message/SSVLSRGQXZSLN1" className="fa fa-whatsapp"></a>
 
             </section>
         </div>
